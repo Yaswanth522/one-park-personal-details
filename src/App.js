f5702654392a18948118dcd985b2bf4958e9c7df
Jadapalli Yaswanth <52879221+Yaswanth522@users.noreply.github.com>
2024-04-17 18:37:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,27 +3,20 @@ import { useState } from "react";
 
 function App() {
   const [formData, setFormData] = useState({
-    "company-name": "Yellow.ai",
-    "business-type": "Limited Liability Company(LLC)",
-    "industry": "Others"
+    name: "Vasudev Chatti",
+    email: "vasu@yellow.ai",
+    phone: "+15715278608"
   });
 
   const onChangeHandler = (e) => {
-    let returnedValue = e.target.value;
-    // if (
-    //   e.target.name === "have-checking-account" ||
-    //   e.target.name === "active-bankruptcy"
-    // ) {
-    //   returnedValue = returnedValue === "on" ? "Yes" : "No";
-    // }
     setFormData({
       ...formData,
-      [e.target.name]: returnedValue,
+      [e.target.name]: e.target.value,
     });
   };
+
   const onSubmitHandler = (e) => {
     e.preventDefault();
-    // console.log(formData)
     window.parent.postMessage(
       JSON.stringify({
         event_code: "ym-client-event",
@@ -40,16 +33,16 @@ function App() {
 
   return (
     <div className="container">
-      <h2 className="form-heading">Company information </h2>
+      <h3 className="form-heading">Contact information</h3>
       <form onSubmit={onSubmitHandler}>
         <div className="row">
-        <label htmlFor="company-name">Name</label>
+          <label htmlFor="name">Full Name</label>
           <div className="tf">
             <input
               type="text"
-              id="company-name"
-              name="company-name"
-              value={formData["company-name"]}
+              id="name"
+              name="name"
+              value={formData.name}
               onChange={onChangeHandler}
               required
             />
@@ -57,131 +50,34 @@ function App() {
           </div>
         </div>
         <div className="row">
-          <label htmlFor="business-type">Business Type</label>
-          <div>
-            <select
-              value={formData["business-type"]}
-              className="form-inputs"
-              name="current-business-structure"
+        <label htmlFor="phone">Phone number</label>
+          <div className="tf">
+            <input
+              type="tel"
+              id="phone"
+              name="phone"
+              value={formData.phone}
               onChange={onChangeHandler}
               required
-            >
-              <option value="Sole Proprietorship">Sole Proprietorship</option>
-              <option value="Partnership">Partnership</option>
-              <option value="Limited Liability Company(LLC)">
-                Limited Liability Company(LLC)
-              </option>
-              <option value="C Corporation">C Corporation</option>
-              <option value="S Corporation">S Corporation</option>
-              <option value="Others">Others</option>
-            </select>
+            />
+            <br />
           </div>
         </div>
         <div className="row">
-        <label htmlFor="industry">Industry</label>
+        <label htmlFor="name">Email</label>
           <div className="tf">
-            <select
-              value={formData.industry}
-              className="form-inputs"
-              name="industry"
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
               onChange={onChangeHandler}
               required
-            >
-              <option value="Restaurant">Restaurant</option>
-              <option value="Healthcare">Healthcare</option>
-              <option value="Construction">Construction</option>
-              <option value="Hospitality">Hospitality</option>
-              <option value="Others">Others</option>
-            </select>
+            />
+            <br />
           </div>
         </div>
-        {/* <div className="row">
-          <label htmlFor="state">State</label>
-          <div>
-            <select
-              value={formData.state}
-              className="form-inputs"
-              name="state"
-              onChange={onChangeHandler}
-              required
-            >
-              <option value="AL">Alabama</option>
-              <option value="AK">Alaska</option>
-              <option value="AZ">Arizona</option>
-              <option value="AR">Arkansas</option>
-              <option value="CA">California</option>
-              <option value="CO">Colorado</option>
-              <option value="CT">Connecticut</option>
-              <option value="DE">Delaware</option>
-              <option value="DC">District Of Columbia</option>
-              <option value="FL">Florida</option>
-              <option value="GA">Georgia</option>
-              <option value="HI">Hawaii</option>
-              <option value="ID">Idaho</option>
-              <option value="IL">Illinois</option>
-              <option value="IN">Indiana</option>
-              <option value="IA">Iowa</option>
-              <option value="KS">Kansas</option>
-              <option value="KY">Kentucky</option>
-              <option value="LA">Louisiana</option>
-              <option value="ME">Maine</option>
-              <option value="MD">Maryland</option>
-              <option value="MA">Massachusetts</option>
-              <option value="MI">Michigan</option>
-              <option value="MN">Minnesota</option>
-              <option value="MS">Mississippi</option>
-              <option value="MO">Missouri</option>
-              <option value="MT">Montana</option>
-              <option value="NE">Nebraska</option>
-              <option value="NV">Nevada</option>
-              <option value="NH">New Hampshire</option>
-              <option value="NJ">New Jersey</option>
-              <option value="NM">New Mexico</option>
-              <option value="NY">New York</option>
-              <option value="NC">North Carolina</option>
-              <option value="ND">North Dakota</option>
-              <option value="OH">Ohio</option>
-              <option value="OK">Oklahoma</option>
-              <option value="OR">Oregon</option>
-              <option value="PA">Pennsylvania</option>
-              <option value="RI">Rhode Island</option>
-              <option value="SC">South Carolina</option>
-              <option value="SD">South Dakota</option>
-              <option value="TN">Tennessee</option>
-              <option value="TX">Texas</option>
-              <option value="UT">Utah</option>
-              <option value="VT">Vermont</option>
-              <option value="VA">Virginia</option>
-              <option value="WA">Washington</option>
-              <option value="WV">West Virginia</option>
-              <option value="WI">Wisconsin</option>
-              <option value="WY">Wyoming</option>
-            </select>
-          </div>
-        </div> */}
-        {/* <div className="row">
-          <label htmlFor="have-checking-account">
-            Do you have a business checking account?
-          </label>
-          <div className="tf">
-            <label className="switch">
-              <input type="checkbox" name="have-checking-account" onChange={onChangeHandler} />
-              <span className="slider round"></span>
-            </label>
-          </div>
-        </div>
-        <div className="row">
-          <label htmlFor="active-bankruptcy">
-            Are you currently in an active bankruptcy?
-          </label>
-          <div className="tf">
-            <label className="switch">
-              <input type="checkbox" name="active-bankruptcy" onChange={onChangeHandler} />
-              <span className="slider round"></span>
-            </label>
-          </div>
-        </div> */}
-        <button type="submit">Next</button>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
